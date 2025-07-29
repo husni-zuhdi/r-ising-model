eec1869e316271c269824d6a052961bf26ee28c2
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2025-07-29 23:25:43 +0700

    feat: the tui works

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,53 @@
-use core::{f64, time};
+use core::f64;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     buffer::Buffer,
-    layout::{Alignment, Layout, Rect},
-    style::Stylize,
+    layout::Rect,
+    style::{Color, Stylize},
     symbols::border,
-    text::{Line, Span, Text},
-    widgets::{
-        canvas::{Canvas, Points},
-        Block, BorderType, Paragraph, Widget,
-    },
+    text::Line,
+    widgets::{Block, BorderType, Paragraph, Widget},
     DefaultTerminal, Frame,
 };
-use std::io;
-use std::thread::sleep;
+use std::time::Instant;
+use std::{io, time::Duration};
 
-const UP: &str = "+";
-const DOWN: &str = "-";
-const J: f64 = 10.0; // Interactivity. J > 0 ferromagnetic. J = 0 non-interactive. J < 0
-                     // anti-ferromagnetic
-const TEMP: f64 = 10000.0; // In K
 const KB: f64 = 1.380649e-23; // Boltzmann Constant in J K^-1
 
 #[derive(Debug, Default)]
 struct App {
     lattice: Lattice,
+    increment: f64,
+    delay: Duration,
     exit: bool,
 }
 
 impl App {
     /// Run app until user quit
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
-        // Init lattice and run the app loop
-        self.lattice = Lattice::new(25, J, TEMP);
+        // Init lattice and values
+        let init_interactivity = 10_000.0;
+        let init_temperature = 10_000.0;
+        self.increment = 1000.0;
+        self.delay = Duration::from_millis(10);
+        let mut last_tick = Instant::now();
+
+        self.lattice = Lattice::new(25, init_interactivity, init_temperature);
+
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
+
+            // Start event pooling
+            let timeout = self.delay.saturating_sub(last_tick.elapsed());
+            if event::poll(timeout)? {
+                self.handle_events()?
+            }
+
+            // Update lattice after delay
+            if last_tick.elapsed() >= self.delay {
+                self.on_tick();
+                last_tick = Instant::now()
+            }
         }
         Ok(())
     }
@@ -60,32 +72,88 @@ impl App {
 
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Char('i') => self.exit(),
-            KeyCode::Char('t') => self.exit(),
+            KeyCode::Char('q') | KeyCode::Char('Q') => self.exit(),
+            KeyCode::Char('+') => self.increase_increment(),
+            KeyCode::Char('-') => self.decrease_increment(),
+            KeyCode::Char('i') => self.increase_interactivity(),
+            KeyCode::Char('t') => self.increase_temperature(),
+            KeyCode::Char('d') => self.increase_delay(),
+            KeyCode::Char('I') => self.decrease_interactivity(),
+            KeyCode::Char('T') => self.decrease_temperature(),
+            KeyCode::Char('D') => self.decrease_delay(),
             _ => {}
         }
     }
 
+    // Run Metropolis Algorithm after delay second
+    fn on_tick(&mut self) {
+        let (x_rand, y_rand) = self.lattice.pick_random_point();
+        self.lattice.metropolis_algo_calculation(x_rand, y_rand);
+    }
+
     fn exit(&mut self) {
         self.exit = true;
+    }
+
+    fn increase_interactivity(&mut self) {
+        self.lattice.interactivity = self.lattice.interactivity + self.increment
+    }
+
+    fn increase_temperature(&mut self) {
+        self.lattice.temperature = self.lattice.temperature + self.increment
+    }
+
+    fn increase_increment(&mut self) {
+        self.increment = self.increment + 10.0
+    }
+
+    fn increase_delay(&mut self) {
+        self.delay = self.delay + Duration::from_millis(10)
+    }
+
+    fn decrease_interactivity(&mut self) {
+        self.lattice.interactivity = self.lattice.interactivity - self.increment
+    }
+
+    fn decrease_temperature(&mut self) {
+        self.lattice.temperature = self.lattice.temperature - self.increment
+    }
+
+    fn decrease_increment(&mut self) {
+        self.increment = self.increment - 10.0
+    }
+
+    fn decrease_delay(&mut self) {
+        if self.delay != Duration::from_millis(0) {
+            self.delay = self.delay - Duration::from_millis(10)
+        } else {
+            self.delay = Duration::from_millis(0)
+        }
     }
 }
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from("The r-ising model".bold());
+        let interactivity = self.lattice.interactivity;
+        let temperature = self.lattice.temperature;
+        let increment = self.increment;
+        let delay = self.delay.as_millis();
+
         let instructions = Line::from(vec![
-            " Quit ".into(),
-            " <Q> ".red().bold(),
-            " Interactivity ".into(),
-            " <I> ".yellow().bold(),
-            " Temperature ".into(),
-            " <T> ".blue().bold(),
+            " Interactivity".into(),
+            format!(" = {:.2}", interactivity).yellow().bold(),
+            " Temperature".into(),
+            format!(" = {:.2} K", temperature).blue().bold(),
+            " Variable Increment".into(),
+            format!(" = {:.2}", increment).red(),
+            " Delay Increment".into(),
+            format!(" = {:.2} ms", delay).red(),
         ]);
 
         let block = Block::bordered()
             .title(title.centered())
+            .title(Line::from(" Quit <q/Q> ").red().bold().left_aligned())
             .title_bottom(instructions.centered())
             .border_set(border::THICK)
             .border_type(BorderType::Rounded);
@@ -97,10 +165,10 @@ impl Widget for &App {
             for x in y_text {
                 match x.as_str() {
                     "-1" => {
-                        x_row.push(" . ".blue());
+                        x_row.push(" v ".fg(Color::Yellow).bg(Color::White));
                     }
                     "1" => {
-                        x_row.push(" * ".red());
+                        x_row.push(" ^ ".fg(Color::Yellow).bg(Color::Red));
                     }
                     _ => {
                         continue;
@@ -118,7 +186,9 @@ impl Widget for &App {
 
 #[derive(Clone, Debug, Default)]
 struct Lattice {
+    // the 3d lattice
     value: Vec<Vec<i32>>,
+    // lattice size
     size: usize,
     interactivity: f64,
     temperature: f64,
@@ -143,18 +213,7 @@ impl Lattice {
         }
     }
 
-    fn set_size(&mut self, size: usize) {
-        self.size = size
-    }
-
-    fn set_temperature(&mut self, temperature: f64) {
-        self.temperature = temperature
-    }
-
-    fn set_interactivity(&mut self, interactivity: f64) {
-        self.interactivity = interactivity
-    }
-
+    // convert 1 and 0 to String
     fn to_string(self) -> Vec<Vec<String>> {
         let mut lattice: Vec<Vec<String>> = Vec::new();
 
@@ -169,25 +228,7 @@ impl Lattice {
         lattice
     }
 
-    fn show(&self) {
-        for y_index in 0..self.size {
-            for x_index in 0..self.size {
-                let spin = if self.value[y_index][x_index] == 1 {
-                    UP
-                } else {
-                    DOWN
-                };
-                if x_index == self.size - 1 {
-                    // Add a new line
-                    println!(" {} ", spin)
-                } else {
-                    // Only print spin
-                    print!(" {} ", spin)
-                }
-            }
-        }
-    }
-
+    // pick randomg x and y point to be flipped
     fn pick_random_point(&self) -> (usize, usize) {
         (
             rand::random_range(0..self.size),
@@ -197,7 +238,7 @@ impl Lattice {
 
     fn calculate_hamiltonian(&self, x_rand: usize, y_rand: usize) -> f64 {
         let random_spin = f64::from(self.value[y_rand][x_rand]);
-        println!("Random Point at {}, {}: {}", x_rand, y_rand, random_spin);
+        // println!("Random Point at {}, {}: {}", x_rand, y_rand, random_spin);
 
         let left = if x_rand != 0 {
             self.value[y_rand][x_rand - 1]
@@ -219,10 +260,10 @@ impl Lattice {
         } else {
             self.value[y_rand][x_rand]
         };
-        println!(
-            "Left: {}, Right: {}, Down: {}, Up: {}.",
-            left, right, up, down
-        );
+        // println!(
+        //     "Left: {}, Right: {}, Down: {}, Up: {}.",
+        //     left, right, up, down
+        // );
         // Hamiltonian Formula
         // H = -J * sum_over_nearest_neighbors(spin_i, spin_j)
         // H = -J * current_spin * sum_of_all_neighbors
@@ -239,34 +280,23 @@ impl Lattice {
         // Else keep the old spin
         let current_hamiltonian_energy = self.calculate_hamiltonian(x_rand, y_rand);
         let new_hamiltonian_energy = -1.0 * current_hamiltonian_energy;
-        println!(
-            "New and Current H: {}, {}",
-            new_hamiltonian_energy, current_hamiltonian_energy
-        );
+        // println!(
+        //     "New and Current H: {}, {}",
+        //     new_hamiltonian_energy, current_hamiltonian_energy
+        // );
 
         let delta_h = new_hamiltonian_energy - current_hamiltonian_energy;
         let acceptence_criteria = f64::consts::E.powf(delta_h * (-1.0 / KB * self.temperature));
-        println!("Delta H: {}", delta_h);
-        println!("A: {}", acceptence_criteria);
+        // println!("Delta H: {}", delta_h);
+        // println!("A: {}", acceptence_criteria);
 
         // Flip only in these two condition
         if delta_h < 0.0 || acceptence_criteria > 0.5 {
             self.value[y_rand][x_rand] = self.value[y_rand][x_rand] * -1;
-            println!("FLIPPED!")
+            // println!("FLIPPED!")
         }
     }
 }
-
-// fn main() {
-//     let mut lattice = Lattice::new(5, J, TEMP);
-//
-//     for _ in 0..50 {
-//         lattice.show();
-//         let (x_rand, y_rand) = lattice.pick_random_point();
-//         lattice.metropolis_algo_calculation(x_rand, y_rand);
-//         sleep(time::Duration::from_secs(1))
-//     }
-// }
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
